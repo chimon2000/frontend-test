e3cfdc90da5da88e472b6c993095c7cab3ac7377
Ryan <ryan.c.edge@outlook.com>
2019-09-27 18:47:22 -0400

    Added some utility styles

---
+++ src/styles/util.ts
@@ -0,0 +1,13 @@
+import { css } from "emotion";
+
+export const displayFlexCls = css`
+  display: flex;
+`;
+
+export const alignItemsCenterCls = css`
+  align-items: center;
+`;
+
+export const transformUppercaseCls = css`
+  text-transform: uppercase;
+`;
