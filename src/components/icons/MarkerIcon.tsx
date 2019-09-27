29687e6f742ca7f3ceffb80a53da26ee9c45a81c
Ryan <ryan.c.edge@outlook.com>
2019-09-27 17:22:19 -0400

    Added marker icon

---
+++ src/components/icons/MarkerIcon.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export function MarkerIcon({ className }: { className? }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 42 66"
+      className={className}>
+      <path
+        fill="#002B56"
+        fillRule="evenodd"
+        d="M38.88 32.02A20.902 20.902 0 0042 21C42 9.402 32.598 0 21 0S0 9.402 0 21c0 4.102 1.176 7.93 3.21 11.164l16.263 32.724c.74 1.49 2.871 1.477 3.593-.022L38.88 32.019z"
+        clipRule="evenodd"
+      />
+      <path
+        fill="#fff"
+        fillRule="evenodd"
+        d="M21 35c7.732 0 14-6.268 14-14S28.732 7 21 7 7 13.268 7 21s6.268 14 14 14z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
+
+export default MarkerIcon;
